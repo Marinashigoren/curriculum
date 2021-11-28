19f92f04b13048fb4ec10239737e9beba4d346bd
Marinashigoren <77338590+Marinashigoren@users.noreply.github.com>
2021-11-28 18:41:49 +0900

    ピラミッド(2回目)
    松井さん
    ピラミッドのプログラム実施してみました。
    ご確認の程よろしくお願い致します。

--- JavaStady/Pyramids-MadeofStar/src/Study/Main.java
+++ JavaStady/Pyramids-MadeofStar/src/Study/Main.java
@@ -3,14 +3,13 @@ package Study;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		for(int i=0; i<3; i++) {
-			for(int space=0; space<=i+1; space++) {
-				System.out.print("");
-				for(int hoshi=0; hoshi<i; hoshi++);{
+		for(int i=0, line=9;  i<line; i++) {
+			for(int space=line-1; i<space; space--) {
+				System.out.print("　");
+			}
+				for(int hoshi=0; hoshi<(i+1)*2-1; hoshi++){
 					System.out.print("★");
 				}
-			}
 			System.out.println();
 		}
 	}
